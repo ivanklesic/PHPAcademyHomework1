95058b587f3e9f86e2dafe3ab0788f2fe3eddf41
ivanklesic <ivan.klesic@gmail.com>
2020-09-03 19:40:57 +0200

    Homework 1 done

---
+++ elements.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'include/header.php';
+include_once 'include/navbar.php';
+?>
+    <div class="uk-section">
+        <div class="uk-container">
+            <form>
+                <select class="uk-select">
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                </select>
+                <textarea class="uk-textarea" placeholder="This is a text area element"></textarea>
+
+                <input class="uk-checkbox" type="checkbox">
+                <input class="uk-checkbox" type="checkbox">
+                <input class="uk-checkbox" type="checkbox">
+
+            </form>
+        </div>
+    </div>
+
+<?php
+require_once 'include/footer.php';
+?>
